1070d10033cdcfd24a26ce92d9baa34b492dcc64
Philipp Rothenhäusler <phirot@kth.se>
2020-11-25 16:45:06 +0100

    Add Python setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylevel",
+    version="0.0.8",
+    author="Frank Jiang, Philipp Rothenhäusler",
+    author_email="fjiang@kth.se, philipp.rothenhaeusler@gmail.com",
+    description="Python package for MATLAB based Levelset toolbox.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KTH-SML/SML_level_set.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
