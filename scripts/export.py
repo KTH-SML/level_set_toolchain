f5080439dba066a688b408a9faf9b20efa0d3cec
Philipp Rothenhäusler <phirot@kth.se>
2021-04-20 19:01:01 +0200

    Add export import example

---
+++ scripts/export.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+""" Example for level set wrapper usage.
+
+    Author: Philipp Rothenhäusler, Stockholm 2020
+
+"""
+
+
+import numpy
+
+
+import pylevel
+
+
+LABEL = "ExampleLevelSet"
+FROM_MEMORY = True
+EXEMPLIFY_DEBUG_VERBOSITY = True
+
+if __name__ == '__main__':
+    level_set_type = pylevel.datasets.LevelSetExample.Drone
+
+    if not FROM_MEMORY:
+        wrapper = pylevel.wrapper.ReachableSetWrapper(
+                label=LABEL,
+                path=pylevel.datasets.path[level_set_type],
+                debug_is_enabled=EXEMPLIFY_DEBUG_VERBOSITY)
+        wrapper.export()
+    else:
+        wrapper = pylevel.wrapper.ReachableSetWrapper(
+                label=LABEL,
+                from_memory=".",
+                debug_is_enabled=EXEMPLIFY_DEBUG_VERBOSITY)
+
+        ## Time steps from final time tf to t0
+        t_idx = list(wrapper.time)
+        t_idx.reverse()
+
+        ## Time indexed access of levelsets
+        levelsets_states = [wrapper.reach_at_t(t) for t in t_idx]
+        for levelset_states, t in zip(levelsets_states,t_idx):
+            states_sliced = numpy.hstack([
+                levelset_states[:, [0]],
+                levelset_states[:, [2]]])
+            pylevel.utilities.visualise_2d(wrapper, states_sliced, t, show=False, show_image=False)
+            pylevel.utilities.show_plots_for_time(time_in_seconds=.5)
+
+        pylevel.utilities.show_plots()
+
--- scripts/timed_level_sets.py
+++ scripts/timed_level_sets.py
@@ -12,24 +12,27 @@ import numpy
 import pylevel
 
 
+EXEMPLIFY_DEBUG_VERBOSITY = True
+
+
 if __name__ == '__main__':
-    ## Show exemplified enum to path usage from pylevel.datasets module
     level_set_type = pylevel.datasets.LevelSetExample.Drone
 
-    wrapper = pylevel.wrapper.LevelSetWrapper(
+    wrapper = pylevel.wrapper.ReachableSetWrapper(
             label="ExampleLevelSet",
-            path=pylevel.datasets.path[level_set_type])
+            path=pylevel.datasets.path[level_set_type],
+            debug_is_enabled=EXEMPLIFY_DEBUG_VERBOSITY)
 
     ## Time steps from final time tf to t0
     t_idx = list(wrapper.time)
     t_idx.reverse()
 
     ## Time indexed access of levelsets
-    levelsets_states = [wrapper.get_reachable_set_at_time(t) for t in t_idx]
-    for levelset_states, t in zip(levelsets_states,t_idx):
+    reachable_states = [wrapper.reach_at_t(t) for t in t_idx]
+    for reachable_states, t in zip(reachable_states,t_idx):
         states_sliced = numpy.hstack([
-            levelset_states[:, [0]],
-            levelset_states[:, [2]]])
+            reachable_states[:, [0]],
+            reachable_states[:, [2]]])
         pylevel.utilities.visualise_2d(wrapper, states_sliced, t, show=False, show_image=False)
         pylevel.utilities.show_plots_for_time(time_in_seconds=.5)
 
