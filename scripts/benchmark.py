b0ed83ff3ccc52a469939aff4e001ffa13fa290e
Philipp Rothenhäusler <phirot@kth.se>
2021-05-24 12:54:18 +0200

    Update example scripts
    Add forced initialisation, add varying density sets

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -12,14 +12,15 @@ import numpy
 import pylevel
 
 
+# FORCE_INITIALISATION = False
 FORCE_INITIALISATION = True
 EXEMPLIFY_DEBUG_VERBOSITY = True
 
 
 if __name__ == '__main__':
-    #level_set_type = pylevel.datasets.LevelSetExample.DroneForesight
     # level_set_type = pylevel.datasets.LevelSetExample.BenchmarkLowResLowHoriz
     level_set_type = pylevel.datasets.LevelSetExample.BenchmarkMediumResMediumHoriz
+    # level_set_type = pylevel.datasets.LevelSetExample.BenchmarkHighResMediumHoriz
 
     wrapper = pylevel.wrapper.ReachableSetWrapper(
             label="ExampleLevelSet",
@@ -30,18 +31,19 @@ if __name__ == '__main__':
     ## Time steps from final time tf to t0
     t_idx = list(wrapper.time)
     t_idx.reverse()
+    t_idx_sel = t_idx[0]
 
-    ## TODO: Check membership of state set
     ## Time indexed access of levelsets
-    print('Has length: ', len(t_idx))
-    reachable_states = [wrapper.reach_at_t(t) for t in t_idx[:-1]]
-    for reachable_states, t in zip(reachable_states,t_idx[:-1]):
-        states_sliced = reachable_states
-        #states_sliced = numpy.hstack([
-        #    reachable_states[:, [0]],
-        #    reachable_states[:, [2]]])
-        pylevel.utilities.visualise_2d(wrapper, states_sliced, t, show=False, show_image=False)
-        pylevel.utilities.show_plots_for_time(time_in_seconds=.5)
+    print('Time horizon :\n', t_idx)
+    print('--> Select time index : ', t_idx_sel)
+    reachable_states = wrapper.reach_at_t(t_idx_sel)
+    states_sliced = reachable_states
+    # states_sliced = numpy.hstack([
+    #     reachable_states[:, [0]],
+    #     reachable_states[:, [2]]])
+
+    pylevel.utilities.visualise_2d(wrapper, states_sliced, t_idx_sel, show=False, show_image=False)
+    pylevel.utilities.show_plots_for_time(time_in_seconds=.5)
 
     pylevel.utilities.show_plots()
 
